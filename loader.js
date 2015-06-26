215c57f2bbf21d64bc4e9489037709a8b2fe816f
Roman Melnyk <rmelnyk@libertyglobal.com>
2015-06-26 18:22:05 +0200

    Loader improvement: file extension recognition

--- loader.js
+++ loader.js
@@ -1,27 +1,43 @@
 (function () {
-	function Promise () {
-		this.filesToLoad = {};
-		this.loadedFiles = {};
-		this.callback = function () {};
-		this.then = function (cb) {
-			if (typeof  cb === 'function') {
-				if (Object.keys(this.filesToLoad).length > 0) {
-					this.callback = cb;
-				} else {
-					cb(this.loadedFiles);
-				}
-			}
-		};
+	var IMAGE_EXTENSIONS = ('jpg,jpeg,gif,bmp,png').split(',');
+	var VIDEO_EXTENSIONS = ('mpg,mpeg,mp4,flv,ogg').split(',');
+
+	function getExtension (filename) {
+		var ext = /\.([^.]*$)/i.exec(filename);
+		ext = ext ? ext[1] : '';
+		return ext.toLowerCase();
+	}
+
+	function isImage (ext) {
+		return IMAGE_EXTENSIONS.indexOf(ext) > -1;
+	}
+
+	function isVideo (ext) {
+		return VIDEO_EXTENSIONS.indexOf(ext) > -1;
 	}
 
 	var Loader = window.Loader = {
 		/**
-		 * @param {...String} asset					one or more filenames to load
+		 * @param {...String} assets					one or more filenames to load
 		 */
-		load: function (asset) {
+		load: function (assets) {
 			var filesToLoad = {};
 			var loadedFiles = {};
 			var callback = function () {};
+
+			Array.prototype.slice.call(arguments).forEach(function (asset) {
+				var ext = getExtension(asset);
+
+				if (isImage(ext)) {
+					// load image
+					filesToLoad[asset] = {type: 'image'};
+				} else if (isVideo(ext)) {
+					// load video
+					filesToLoad[asset] = {type: 'video'};
+				} else {
+					console.log('[ WARN ] Loader: unknown asset type: "' + asset + '"; skipping');
+				}
+			});
 
 			return {
 				then: function (cb) {
@@ -35,7 +51,6 @@
 				}
 			};
 		},
-
 
 		test: function () {
 			document.querySelector('#content').innerHTML = 'Hello!';
