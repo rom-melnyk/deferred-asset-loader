5d91ff7c9d263059af4023124e330197caf432a3
Roman Melnyk <rmelnyk@libertyglobal.com>
2015-06-26 18:46:45 +0200

    Loader: timeout added; asset distinguishment

--- loader.js
+++ loader.js
@@ -1,6 +1,7 @@
 (function () {
 	var IMAGE_EXTENSIONS = ('jpg,jpeg,gif,bmp,png').split(',');
 	var VIDEO_EXTENSIONS = ('mpg,mpeg,mp4,flv,ogg').split(',');
+	var LOADING_TIMEOUT = 5 * 1000; // 5 sec
 
 	function getExtension (filename) {
 		var ext = /\.([^.]*$)/i.exec(filename);
@@ -25,19 +26,59 @@
 			var loadedFiles = {};
 			var callback = function () {};
 
-			Array.prototype.slice.call(arguments).forEach(function (asset) {
-				var ext = getExtension(asset);
+			/**
+			 * @private
+			 * The hook for "file-is-loaded" event
+			 */
+			function __onload () {
+				if (Object.keys(filesToLoad).length === 0) {
+					callback(loadedFiles);
+				}
+			}
 
-				if (isImage(ext)) {
-					// load image
+			Array.prototype.slice.call(arguments).forEach(function (asset) {
+				var ext = getExtension(asset),
+					element;
+
+				if (isImage(ext)) { // ------------ load image ------------
 					filesToLoad[asset] = {type: 'image'};
-				} else if (isVideo(ext)) {
-					// load video
+
+					element = document.createElement('image');
+					element.crossOrigin = element.crossorigin = 'anonymous';
+					element.onload = function () {
+						delete filesToLoad[src];
+						loadedFiles[asset] = element;
+						__onload();
+					};
+					element.src = src;
+
+				} else if (isVideo(ext)) { // ------------ load video ------------
 					filesToLoad[asset] = {type: 'video'};
+
+					element = document.createElement('image');
+					element.crossOrigin = element.crossorigin = 'anonymous';
+					element.preload = 'auto';
+					element.oncanplay = function () {
+						delete filesToLoad[src];
+						loadedFiles[asset] = element;
+						__onload();
+					}
 				} else {
 					console.log('[ WARN ] Loader: unknown asset type: "' + asset + '"; skipping');
 				}
 			});
+
+			setTimeout(function () {
+				if (Object.keys(filesToLoad).length > 0) {
+					console.log('[ WARN ] Not all the resources were loaded in time:');
+					for (var asset in filesToLoad) {
+						console.log('   > ' + asset);
+						loadedFiles[asset] = null;
+					}
+					filesToLoad = {};
+					__onload();
+				}
+			}, LOADING_TIMEOUT);
 
 			return {
 				then: function (cb) {
